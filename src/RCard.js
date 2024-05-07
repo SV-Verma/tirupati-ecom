1525a89eafd3f666242d76ca6e8405eb8dd1c2f2
shivangi <shivangivv07@gmail.com>
2024-05-07 15:49:58 +0530

    hlool

--- src/RCard.js
+++ src/RCard.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const RCard = (props) => {
     return (
-<>
-        <div className="Card-main">
-            <div className="card-imgs">
-                <Link to={props.link}>
-                <img src={props.imgsrc} alt={props.imgsrc} />
-                <div className="card-text">
-                    <p>{props.text}</p>
-                    <span>{props.price}</span>
+        <>
+            <div className="Card-main">
+                <div className="card-imgs">
+                    <Link to={props.link}>
+                        <img src={props.imgsrc} alt={props.imgsrc} />
+                        <div className="card-text">
+                            <p>{props.text}</p>
+                            <span>{props.price}</span>
+                        </div>
+                    </Link>
                 </div>
-                </Link>
             </div>
-        </div>
         </>
     )
 }
--- src/RecomendP.js
+++ src/RecomendP.js
@@ -3,20 +3,20 @@ import Card from './RCard'
 import CRItems from './CRItems'
 
 const RecomendP = () => {
-  return ( 
+  return (
     <>
-    <div className='recomend'>
-     {
-   CRItems.map((val, ind) =>{
-    return <Card key={ind}
-     imgsrc={val.imgsrc}
-     text={val.text}
-     price={val.price}
-    link={val.link}
-    />
-   })
- }
-    </div>
+      <div className='recomend'>
+        {
+          CRItems.map((val, ind) => {
+            return <Card key={ind}
+              imgsrc={val.imgsrc}
+              text={val.text}
+              price={val.price}
+              link={val.link}
+            />
+          })
+        }
+      </div>
     </>
   )
 }
